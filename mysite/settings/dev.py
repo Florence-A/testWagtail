5332e3c472c614140654ce3e072aaa28867f273b
Florence <fatoumatacamara@airdefatoumata.home>
2022-11-09 15:26:41 +0100

    first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-mjyx#!*40d&y9l$#hg()c+9kdxona!e8-m2d50+#+=m)h#trkq"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
